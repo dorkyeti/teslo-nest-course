30c6f38a2b78b46fe5e332c503a11ad55bf2d1f0
Wally <wjmalave@gmail.com>
2022-10-26 19:09:38 -0400

    remove mime

--- src/files/helpers/file-filter.helper.ts
+++ src/files/helpers/file-filter.helper.ts
@@ -1,10 +1,9 @@
-const mime = require('mime');
 
 export const fileFilter = (_: Express.Request, file: Express.Multer.File, cb: Function): void => {
     if (!file)
         return cb(new Error('El archivo está vacio'), false);
 
-    const ext = mime.getExtension(file.mimetype);
+    const ext = 'png';
 
     if (['jpg', 'jpeg', 'png', 'gif'].includes(ext))
         return cb(null, true);
--- src/files/helpers/file-namer.helper.ts
+++ src/files/helpers/file-namer.helper.ts
@@ -1,8 +1,7 @@
-import mime = require('mime');
 import { v4 as uuid } from 'uuid';
 
 export const fileNamer = (_: Express.Request, file: Express.Multer.File, cb: Function): void => {
-    const ext = mime.getExtension(file.mimetype);
+    const ext = 'png';
 
     const filename = `${uuid()}.${ext}`;
 
